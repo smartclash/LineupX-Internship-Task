aaf3f738bef922e6f72f95e8d869f74206334ee9
Alpha Man <xalphamanx@tuta.io>
2020-04-28 15:24:15 +0530

    Integrate Webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,12 @@
 const mix = require('laravel-mix');
-const path = require('path');
+const {join} = require('path');
 require('laravel-mix-purgecss');
 
-const OUTPUT_DIR = path.join(__dirname, '/public/');
-const RESOURCE_DIR = path.join(__dirname, '/src/Resources/');
+const OUTPUT_DIR = join(__dirname, '/public/');
+const RESOURCE_DIR = join(__dirname, '/src/Resources/');
 
-mix.js(path.join(RESOURCE_DIR, '/js/app.js'), OUTPUT_DIR)
-    .sass(path.join(RESOURCE_DIR, '/sass/app.scss'), OUTPUT_DIR)
+mix.js(join(RESOURCE_DIR, '/js/app.js'), OUTPUT_DIR)
+    .sass(join(RESOURCE_DIR, '/sass/app.scss'), OUTPUT_DIR)
     .purgeCss({
-        content: [path.join(RESOURCE_DIR, '/views/**/*.pug')],
+        content: [join(RESOURCE_DIR, '/views/**/*.pug')],
     });
