3d027e15e274c70177c34877d115d39b35020b30
Alpha Man <xalphamanx@tuta.io>
2020-05-23 19:22:05 +0530

    Touchups, here and there

--- src/App/Http/Controllers/AuthController.ts
+++ src/App/Http/Controllers/AuthController.ts
@@ -22,7 +22,7 @@ export const processManagerLogin = async (req: Request, res: Response, args: any
     const email = req.body.email;
     const passw = req.body.password;
 
-    if (!validator.isEmail(email) && validator.isEmpty(passw)) {
+    if (!validator.isEmail(email) || validator.isEmpty(passw)) {
         return res.render('auth/manager', { error: 'Either the email or password is not correct' });
     }
 
